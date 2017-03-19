966a0a1d776a368984d28aea568a1f5417792252
Sean Johnson <sean.johnson@maio.me>
2017-03-18 20:40:40 -0500

    More DNS, add 8090 for the kontrol kite

--- resources.tf
+++ resources.tf
@@ -17,6 +17,10 @@ data "aws_ami" "coreos" {
     }
 }
 
+//
+// A default security group which should control general ingress/egress
+// for all instances
+//
 data "aws_security_group" "default" {
     id = "sg-2f74324a"
 }
@@ -29,6 +33,11 @@ resource "random_id" "mg_smtp_password" {
     byte_length = 16
 }
 
+//
+// Ensure our primary zone exists, but also prevent changes
+// so we do not have to keep resetting root NS records at the
+// registrar.
+//
 resource "aws_route53_zone" "primary" {
     name = "${var.base_domain}."
 
@@ -45,14 +54,18 @@ resource "aws_route53_zone" "primary" {
     }
 }
 
+//
+// Mailgun domain for use w/ Koding for mail sending.
+// Depending on the domain, destruction should be prevented, especially
+// in the case of free (ie., .tk, .ml, etc) domains where spam is common.
 resource "mailgun_domain" "primary" {
     name = "${random_id.mg_smtp_password.keepers.domain_name}"
     spam_action = "disabled"
     smtp_password = "${random_id.mg_smtp_password.hex}"
 
-#    lifecycle {
-#        prevent_destroy = true
-#    }
+    lifecycle {
+        prevent_destroy = true
+    }
 }
 
 resource "aws_route53_record" "mailgun_sending_record_0" {
@@ -79,6 +92,9 @@ resource "aws_route53_record" "mailgun_sending_record_2" {
   records = ["${mailgun_domain.primary.sending_records.2.value}"]
 }
 
+//
+// Creates Mailgun MX records on the domain.
+//
 resource "aws_route53_record" "mailgun_recv_records" {
     zone_id = "${aws_route53_zone.primary.zone_id}"
     name = ""
@@ -90,6 +106,9 @@ resource "aws_route53_record" "mailgun_recv_records" {
     ]
 }
 
+//
+// Ephemeral volume for configurations, compose stacks, etc.
+//
 resource "aws_ebs_volume" "koding_data" {
     availability_zone = "${var.aws_az}"
     size = "${var.ephemeral_size}"
@@ -104,10 +123,14 @@ resource "aws_ebs_volume" "koding_data" {
     }
 }
 
+//
+// LB access
+//
 resource "aws_security_group" "koding_fw" {
     name = "koding_fw"
     description = "FW Rules for Koding access"
 
+    // This is only used by the internal LB
     ingress {
         from_port = 80
         to_port = 80
@@ -115,14 +138,26 @@ resource "aws_security_group" "koding_fw" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    // This is only used by the internal LB
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    // Provided by the Koding Kontrol Kite
+    ingress {
+        from_port = 8090
+        to_port = 8090
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
+//
+// SSH management port - access CIDR should probably be narrow.
+//
 resource "aws_security_group" "coreos_mgmt" {
     name = "coreos_mgmt"
     description = "FW rules for coreos access"
@@ -135,6 +170,10 @@ resource "aws_security_group" "coreos_mgmt" {
     }
 }
 
+//
+// CoreOS SG for inter-cluster scaling / cluster communication,
+// should it ever be necessary.
+//
 resource "aws_security_group" "coreos_etcd" {
     name = "coreos_etcd"
     description = "FW rules for coreos clustering"
@@ -168,6 +207,10 @@ resource "aws_security_group" "coreos_etcd" {
     }
 }
 
+//
+// CoreOS instance on which to run Koding.
+// Root FS should be big enough for a number of Docker containers..
+//
 resource "aws_instance" "koding" {
     ami = "${data.aws_ami.coreos.id}"
     availability_zone = "${var.aws_az}"
@@ -192,6 +235,9 @@ resource "aws_instance" "koding" {
     }
 }
 
+//
+// Main Koding access domain
+//
 resource "aws_route53_record" "koding_instance" {
     zone_id = "${aws_route53_zone.primary.zone_id}"
     name = "koding.${aws_route53_zone.primary.name}"
@@ -202,6 +248,9 @@ resource "aws_route53_record" "koding_instance" {
     ]
 }
 
+//
+// Team access wildcard for koding instance (ie., engineering.koding...)
+//
 resource "aws_route53_record" "koding_instance_wc" {
     zone_id = "${aws_route53_zone.primary.zone_id}"
     name = "*.koding.${aws_route53_zone.primary.name}"
@@ -212,6 +261,35 @@ resource "aws_route53_record" "koding_instance_wc" {
     ]
 }
 
+//
+// dev.koding... stub, just for safety.
+//
+resource "aws_route53_record" "koding_devpg_stub" {
+    zone_id = "${aws_route53_zone.primary.zone_id}"
+    name = "dev.koding.${aws_route53_zone.primary.name}"
+    type = "A"
+    ttl = "120"
+    records = [
+        "${aws_instance.koding.public_ip}"
+    ]
+}
+
+//
+// Wildcard for Koding's dev pages service
+//
+resource "aws_route53_record" "koding_devpg_wc" {
+    zone_id = "${aws_route53_zone.primary.zone_id}"
+    name = "*.dev.koding.${aws_route53_zone.primary.name}"
+    type = "A"
+    ttl = "120"
+    records = [
+        "${aws_instance.koding.public_ip}"
+    ]
+}
+
+//
+// AWS volume attachment - ephemeral data volume to instance
+//
 resource "aws_volume_attachment" "data_volume" {
     device_name = "${var.ephemeral_node}"
     instance_id = "${aws_instance.koding.id}"
